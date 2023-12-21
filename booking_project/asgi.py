4680b1d6bcda1bb60fd4f2d5aeca5931aa75ae9c
binshanb <binshanb77@gmail.com>
2023-12-22 00:23:45 +0530

    asgi updation

--- booking_project/asgi.py
+++ booking_project/asgi.py
@@ -1,24 +1,20 @@
 import os
 import django
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat import routing as routingchat
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_project.settings')
 django.setup()
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        routingchat.websocket_urlpatterns
+    ),
+})
 
-django_asgi_application = get_asgi_application
 
-from chat import routing as routingchat
-
-
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        'websocket': URLRouter(routingchat.websocket_urlpatterns),
-
-    }
-)
 
 
 
