399d7c58c65c3a42a2f87626eaf039a95f3ee9e2
binshanb <binshanb77@gmail.com>
2023-12-28 12:50:11 +0530

    asgi changes

--- booking_project/asgi.py
+++ booking_project/asgi.py
@@ -1,11 +1,13 @@
 import os
 import django
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from chat import routing as routingchat
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_project.settings')
 django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat import routing as routingchat
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
